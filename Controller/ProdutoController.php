f35d2cbd710714f300c97e813f0f1aa245feb9ba
enzo <e2enzo2@gmail.com>
2022-05-16 09:13:45 -0300

    Produto Criado

---
+++ Controller/ProdutoController.php
@@ -0,0 +1,53 @@
+<?php
+
+class ProdutoController 
+{
+    public static function index()
+    {
+        
+        include 'Model/ProdutoModel.php'; 
+        
+        $model = new ProdutoModel(); 
+        $model->getAllRows();
+        include 'View/modules/Produto/ListaProdutos.php'; 
+    }
+
+    public static function form()
+    {
+        include 'Model/ProdutoModel.php'; 
+        $model = new ProdutoModel();
+
+        if(isset($_GET['id'])) 
+            $model = $model->getById( (int) $_GET['id']); 
+            
+
+        include 'View/modules/Produto/FormProduto.php'; 
+    }
+
+    public static function save()
+   
+    {
+       include 'Model/ProdutoModel.php'; 
+
+       $model = new ProdutoModel();
+
+       $model->id =  $_POST['id'];
+       $model->nome = $_POST['nome'];
+       $model->cpf = $_POST['cpf'];
+       $model->data_nascimento = $_POST['data_nascimento'];
+
+       $model->save(); 
+
+       header("Location: /produto"); 
+    }
+
+    public static function delete()
+    {
+        include 'Model/ProdutoModel.php'; 
+        $model = new ProdutoModel();
+
+        $model->delete( (int) $_GET['id'] );
+
+        header("Location: /produto"); 
+    }
+}
